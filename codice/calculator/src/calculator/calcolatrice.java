406f5acb02d505788f3e8ab37c3188c82ddb8030
gloriapasinetti <67326021+gloriapasinetti@users.noreply.github.com>
2021-10-22 12:19:55 +0200

    Update calcolatrice.java

--- codice/calculator/src/calculator/calcolatrice.java
+++ codice/calculator/src/calculator/calcolatrice.java
@@ -1,5 +1,59 @@
 package calculator;
 
 public class calcolatrice {
-
+	
+	public static double somma(double a, double b) {
+		double c;
+		double cArrotondato;
+		c = a+b;
+		cArrotondato = Math.round(c*100.0)/100.0;
+		return(cArrotondato);
+	}
+	
+	public static double moltiplicazione(double a, double b) {
+		double c;
+		c = a*b;
+		return(c);
+		
+	}
+	
+	public static double sottrazione(double a, double b) {
+		double c;
+		double cArrotondato;
+	
+		c= a-b;
+		cArrotondato = Math.round(c*100.0)/100.0;
+		return(cArrotondato);		
+	}
+	
+	public static double divisione(double a, double b) throws Exception {
+		double c = 0;
+		double cArrotondato;
+		
+		if(b!=0) {
+			c= a/b;
+			cArrotondato = Math.round(c*100.0)/100.0;
+			// c = c.toFixed(2);
+			}
+		else
+			throw new Exception( "DIVISIONE IMPOSSIBILE");
+		return(cArrotondato);
+		
+		
+	}
+	
+	public static void main(String args[]) throws Exception {
+		double a = Math.random  ();
+		double b = Math.random();
+		double aArrotondato;
+		double bArrotondato;
+		aArrotondato = Math.round(a*100.0)/100.0;
+		bArrotondato= Math.round(b*100.0)/100.0;
+		System.out.println(aArrotondato + " e " + bArrotondato);
+		System.out.println("Somma: " + somma(aArrotondato,bArrotondato));
+		System.out.println("Sottrazione: " + sottrazione(aArrotondato,bArrotondato));
+		System.out.println("Moltiplicazione: " + moltiplicazione(aArrotondato,bArrotondato));
+		System.out.println("Divisione: " + divisione(aArrotondato,bArrotondato));
+		
+	}
 }
